c7c4eaef52c24d77d98ea3a7a442ea57c57f7220
森陽斗 <junhiro3284@gmail.com>
2024-12-05 16:38:04 +0900

    GamePadの変更

--- Source/Input/GamePad.h
+++ Source/Input/GamePad.h
@@ -22,6 +22,10 @@ public:
 	static const GamePadButton BTN_RIGHT_SHOULDER	= (1 << 13);
 	static const GamePadButton BTN_LEFT_TRIGGER		= (1 << 14);
 	static const GamePadButton BTN_RIGHT_TRIGGER	= (1 << 15);
+	static const GamePadButton KEY_W = (1 << 17);
+	static const GamePadButton KEY_A = (1 << 18);
+	static const GamePadButton KEY_S = (1 << 19);
+	static const GamePadButton KEY_D = (1 << 20);
 
 public:
 	GamePad() {}
